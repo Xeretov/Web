b35c2479c56d62d1fbf17c4cd12dcdff789635df
Xeretov <gioeleamendola@gmail.com>
2024-12-11 12:44:10 +0100

    Update

---
+++ projects/vite/prima_prova/src/components/Card.jsx
@@ -0,0 +1,18 @@
+import './Card.css'
+
+function Card() {
+
+    return (
+        <>
+            <ul>
+                <li><a href="#">Link 1</a></li>
+                <li><a href="#">Link 2</a></li>
+                <li><a href="#">Link 3</a></li>
+                <li><a href="#">Link 4</a></li>
+            </ul>
+        </>
+    )
+
+}
+
+export default Card
