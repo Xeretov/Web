1f93a7f541533d19c95526c370b3017c8f424efa
Xeretov <gioeleamendola@gmail.com>
2025-02-27 09:15:51 +0100

    update

--- ProgettoFinale/front-end/src/components/Navbar.jsx
+++ ProgettoFinale/front-end/src/components/Navbar.jsx
@@ -6,11 +6,11 @@ const CustomNavbar = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand as={Link} to="/">Super Azienda</Navbar.Brand>
+        <Navbar.Brand as={Link} to="/">Super Business</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link as={Link} to="/employees">Dipendenti</Nav.Link>
+            <Nav.Link as={Link} to="/employees">Employees</Nav.Link>
             <Nav.Link as={Link} to="/about">About</Nav.Link>
           </Nav>
         </Navbar.Collapse>
